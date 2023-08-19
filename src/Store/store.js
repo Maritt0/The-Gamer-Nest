c389ee724d9f118b3a85bc3d9135056f73d1a060
Maritt0 <121215555+Maritt0@users.noreply.github.com>
2023-08-19 02:58:09 -0300

    Add files via upload

---
+++ src/Store/store.js
@@ -0,0 +1,25 @@
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
+import counterReducer from '../Features/Counter/counterSlice'
+import shopReducer from '../Features/Shop/shopSlice'
+import cartReducer from '../Features/Cart/cartSlice'
+import { shopApi } from '../Services/shopServices'
+import { setupListeners } from '@reduxjs/toolkit/dist/query'
+import { authApi } from '../Services/authServices'
+import userReducer from '../Features/User/userSlice'
+
+const store = configureStore({
+    reducer: {
+        counterReducer,
+        shopReducer,
+        cartReducer,
+        userReducer,
+        [shopApi.reducerPath]: shopApi.reducer,
+        [authApi.reducerPath]: authApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware().concat(shopApi.middleware,authApi.middleware),
+})
+
+setupListeners(store.dispatch)
+
+export default store
