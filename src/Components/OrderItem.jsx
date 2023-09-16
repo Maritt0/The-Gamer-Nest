9c190c22f23532d0ed63823a7151a19412076b37
Maritt0 <121215555+Maritt0@users.noreply.github.com>
2023-09-16 08:25:19 -0300

    Update OrderItem.jsx

--- src/Components/OrderItem.jsx
+++ src/Components/OrderItem.jsx
@@ -1,55 +1,37 @@
 import { StyleSheet, Text, View } from "react-native";
-import { Feather } from "@expo/vector-icons";
 import React from "react";
-import { colors } from "../Global/Colors";
 
 const OrderItem = ({ order }) => {
-    const total = order.items.reduce(
-        (acc, currentItem) => (acc += currentItem.price * currentItem.quantity),
-        0
-    );
+  const total = order.items.reduce(
+    (acc, currentItem) => (acc += currentItem.price * currentItem.quantity),
+    0
+  );
 
-    return (
-        <View style={styles.card} onPress={() => {}}>
-            <View style={styles.textContainer}>
-                <Text style={styles.text}>
-                    {new Date(order.createdAt).toLocaleString()}
-                </Text>
-                <Text style={styles.text2}>${total}</Text>
-            </View>
-            <Feather name="search" size={30} color="black" />
+  return (
+    <View style={styles.card}>
+      {order.items.map((cartItem) => (
+        <View key={cartItem.id}>
+          <Text>Name: {cartItem.title}</Text>
+          <Text>Quantity: {cartItem.quantity}</Text>
+          <Text>Price: ${cartItem.price}</Text>
         </View>
-    );
+      ))}
+      <Text>Total: ${total}</Text>
+    </View>
+  );
 };
 
 export default OrderItem;
 
 const styles = StyleSheet.create({
-    card: {
-        height: 100,
-        backgroundColor: '#696969',
-        padding: 10,
-        margin: 10,
-        borderWidth: 2,
-        borderRadius: 10,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-    },
-    textContainer: {
-        width: "70%",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        alignItems: "flex-start",
-    },
-    text: {
-        fontFamily: "Josefin",
-        fontSize: 17,
-        color: "#f0ffff",
-    },
-    text2: {
-        fontFamily: "Josefin",
-        fontSize: 19,
-        color: "#f0ffff",
-    },
+  card: {
+    backgroundColor: "#696969",
+    padding: 10,
+    margin: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
 });
