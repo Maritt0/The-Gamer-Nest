a562a7b8d6beee1c617035f1f720b20fba22502f
Maritt0 <121215555+Maritt0@users.noreply.github.com>
2023-09-16 08:23:26 -0300

    Update store.js

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,10 +2,10 @@ import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
 import counterReducer from '../Features/Counter/counterSlice'
 import shopReducer from '../Features/Shop/shopSlice'
 import cartReducer from '../Features/Cart/cartSlice'
+import userReducer from '../Features/User/userSlice'
 import { shopApi } from '../Services/shopServices'
 import { setupListeners } from '@reduxjs/toolkit/dist/query'
 import { authApi } from '../Services/authServices'
-import userReducer from '../Features/User/userSlice'
 
 const store = configureStore({
     reducer: {
@@ -17,7 +17,7 @@ const store = configureStore({
         [authApi.reducerPath]: authApi.reducer,
     },
     middleware: (getDefaultMiddleware) => 
-        getDefaultMiddleware().concat(shopApi.middleware,authApi.middleware),
+        getDefaultMiddleware().concat(shopApi.middleware, authApi.middleware),
 })
 
 setupListeners(store.dispatch)
